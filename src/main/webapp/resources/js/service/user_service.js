ce45a15d5f48f4d3b06b7e6c6e806f4c0b2e33fb
cesbe <cesbe@CABERNAL-PC>
2016-04-03 22:24:24 -0500

    Ready to deploy

--- src/main/webapp/resources/js/service/user_service.js
+++ src/main/webapp/resources/js/service/user_service.js
@@ -5,7 +5,7 @@ App.factory('UserService', ['$http', '$q', function($http, $q){
 	return {
 		
 			fetchAllUsers: function() {
-					return $http.get('http://localhost:8080/prueba_conocimientos/user/')
+					return $http.get('http://localhost:8080/user/')
 							.then(
 									function(response){
 										return response.data;
@@ -18,7 +18,7 @@ App.factory('UserService', ['$http', '$q', function($http, $q){
 			},
 		    
 		    createUser: function(user){
-					return $http.post('http://localhost:8080/prueba_conocimientos/user/', user)
+					return $http.post('http://localhost:8080/user/', user)
 							.then(
 									function(response){
 										return response.data;
@@ -31,7 +31,7 @@ App.factory('UserService', ['$http', '$q', function($http, $q){
 		    },
 		    
 		    updateUser: function(user, id){
-					return $http.put('http://localhost:8080/prueba_conocimientos/user/'+id, user)
+					return $http.put('http://localhost:8080/user/'+id, user)
 							.then(
 									function(response){
 										return response.data;
@@ -44,7 +44,7 @@ App.factory('UserService', ['$http', '$q', function($http, $q){
 			},
 		    
 			deleteUser: function(id){
-					return $http.delete('http://localhost:8080/prueba_conocimientos/user/'+id)
+					return $http.delete('http://localhost:8080/user/'+id)
 							.then(
 									function(response){
 										return response.data;
